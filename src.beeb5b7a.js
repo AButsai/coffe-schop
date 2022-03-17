2e4a0c3016b38da206ec3ef84a98d4e7ddeb8349
AButsai <71347585+AButsai@users.noreply.github.com>
2022-03-17 17:04:22 +0000

    Deploying to gh-pages from @ AButsai/coffe-schop@e561f2c42444ea629063c824cda5d2d29d94591f 🚀

--- src.beeb5b7a.js
+++ src.beeb5b7a.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"/home/runner/work/coffe-schop/coffe-schop/src/images/bg-hero.jpg":[["bg-hero.25a2d068.jpg","hT9C"],"hT9C"],"/home/runner/work/coffe-schop/coffe-schop/src/images/bg-hero@2x.jpg":[["bg-hero@2x.335ff523.jpg","CCf3"],"CCf3"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/coffe-schop/src.1458d0e1.js.map
+},{"/home/runner/work/coffe-schop/coffe-schop/src/images/bg-hero.jpg":[["bg-hero.25a2d068.jpg","hT9C"],"hT9C"],"/home/runner/work/coffe-schop/coffe-schop/src/images/bg-hero@2x.jpg":[["bg-hero@2x.335ff523.jpg","CCf3"],"CCf3"]}],"wzuc":[function(require,module,exports) {
+const e=new Swiper(".swiper",{slidesPerView:2,spaceBetween:40,navigation:{prevEl:".feed-back__button-prev",nextEl:".feed-back__button-next"},pagination:{el:".swiper-pagination",type:"fraction",renderFraction:function(e,n){return`<span class="${e}"></span><span> / </span><cpan class="${n}"></cpan>  People`}},scrollbar:{el:".swiper-scrollbar",dragSize:"30%",draggable:!0}});e.init();
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/slider.js");
+},{"./sass/main.scss":"clu1","./js/slider.js":"wzuc"}]},{},["Focm"], null)
+//# sourceMappingURL=/coffe-schop/src.beeb5b7a.js.map
